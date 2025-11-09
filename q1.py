2c8f512f546fb6e44a623add948d760a86e020c3
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2025-11-09 10:52:37 +0500

    Add logic to check if position is served
    Introduced input handling for number of people and position, and added logic to determine if a given position is served based on a custom sequence. Outputs 'Yes' or 'No' depending on whether the position is in the served list.

--- q1.py
+++ q1.py
@@ -1,1 +1,18 @@
+n = int(input("Enter number of people: "))
+k = int(input("Enter position: "))
+people = []
+served = []
+for i in range(0,n):
+  people.append(i+1)
 
+a = 1
+while (a < len(people)):
+  served.append(a)
+  a = a+a+1
+j = 0
+
+if k in served:
+  print("Yes")   
+else:
+  print("No")
+    
